9060c3a3edd83bda742f839e6b79ecb383f20f4f
rodriggonzalezm <rodrig.gonzalezm@gmail.com>
2025-05-21 19:01:04 -0300

    v3

--- manage.py
+++ manage.py
@@ -1,5 +1,11 @@
-from django.core.management import execute_from_command_line
+#!/usr/bin/env python
+import os
 import sys
 
-if __name__ == '__main__':
+def main():
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sexshop.settings')
+    from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
+
+if __name__ == '__main__':
+    main()
