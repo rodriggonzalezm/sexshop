debb638809380b1743140d14ba67c59c756198cf
Rodrigo González <rodrig.gonzalezm@gmail.com>
2025-07-01 20:05:37 -0300

    Ajustes para producción finalllll

--- tripleequis/wsgi.py
+++ tripleequis/wsgi.py
@@ -1,10 +1,11 @@
 import os
-from os import path
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tripleequis.settings')
 
 application = get_wsgi_application()
-static_root = path.join(path.dirname(__file__), 'staticfiles')
-application = WhiteNoise(application, root=static_root)
+
+# Ruta a STATIC_ROOT desde settings
+from django.conf import settings
+application = WhiteNoise(application, root=settings.STATIC_ROOT)
